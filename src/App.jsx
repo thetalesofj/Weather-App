985ee2df6c7fcd0b8687fb4fe69de0b630dfff91
Jeremiah Haastrup <jeremiahhaastrup1@gmail.com>
2022-12-07 19:15:18 +0000

    debugging and installation of tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,19 @@
 import React, {useState} from 'react';
 import axios from 'axios';
-import {BsFillSunriseFill, BsFillSunsetFill, BsWind} from 'react-icons/bs'
-import {WiHumidity} from 'react-icons/wi'
-import {FaTemperatureHigh, FaTemperatureLow} from 'react-icons/fa'
-import {URL} from './api.js'
+import {BsFillSunriseFill, BsFillSunsetFill, BsWind} from 'react-icons/bs';
+import {WiHumidity} from 'react-icons/wi';
+import {FaTemperatureHigh, FaTemperatureLow} from 'react-icons/fa';
+import {WEATHER_API} from './api.js';
 
 function App() {
   const [data, setData] = useState({});
   const [location, setLocation] = useState("");
-  const imgUrl = `http://openweathermap.org/img/w/${data.weather?data.weather[0].icon:null}.png`;
+  const IMG_URL = `http://openweathermap.org/img/w/${data.weather?data.weather[0].icon:null}.png`;
+  const WEATHER_URL = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=${WEATHER_API}`;
   
   const findLocation = (event) => {
     if (event.key === 'Enter') {
-    axios.get(URL).then((response) => {
+    axios.get(WEATHER_URL).then((response) => {
       setData(response.data)
       console.log(response.data)
     })
@@ -21,8 +22,8 @@ function App() {
   }
 
   return (
-    <div className="app">
-      <div className="search">
+    <div className="">
+      <div className="">
         <input 
         value={location}
         onChange={event => setLocation(event.target.value)}
@@ -34,12 +35,13 @@ function App() {
         <div className="top">
           <div className="location">
             <p>{data.name}</p>
+            {data.sys ? <p>{data.sys.country}</p> : null}
           </div>
           <div className="temp">
             {data.main ? <h1>{data.main.temp.toFixed()}°C</h1> : null}
           </div>
           <div className="description">
-            <img src={imgUrl} alt=""></img> 
+            <img src={IMG_URL} alt=""></img> 
           </div>
         </div>
 
@@ -48,20 +50,24 @@ function App() {
           <div className="tempReadings">
             <div className="tempMax"> 
               <FaTemperatureHigh/>
+              <p>Max: </p>
               {data.main ? <p>{data.main.temp_max.toFixed()}°C</p> : null}
             </div>
             <div className="tempMin">
               <FaTemperatureLow/>
+              <p>Min: </p>
               {data.main ? <p>{data.main.temp_min.toFixed()}°C</p> : null}
             </div>
           </div>
           <div className="sun">
             <div className="sunrise">
               <BsFillSunriseFill/>
+              <p>Rise: </p>
               {data.sys ? <p>{new Date(data.sys.sunrise * 1000).toISOString().slice(-13,-8)}</p> : null}
             </div>
             <div className="sunset">
               <BsFillSunsetFill/>
+              <p>Set: </p>
               {data.sys ? <p>{new Date(data.sys.sunset * 1000).toISOString().slice(-13,-8)}</p> : null}
             </div>
           </div>
@@ -71,15 +77,18 @@ function App() {
       {data.name !== undefined &&
         <div className="bottom2">
           <div className="feels">
+            <FaTemperatureHigh/>
             <p>Feels Like</p>
             {data.main ? <p>{data.main.feels_like.toFixed()}°C</p> : null}
           </div>
           <div className="humidity">
             <WiHumidity/>
+            <p>Humidity: </p>
             {data.main ? <p>{data.main.humidity}%</p> : null}
           </div>
           <div className="wind">
             <BsWind/>
+            <p>Wind: </p>
             {data.wind ? <p>{data.wind.speed.toFixed()} MPH</p> : null}
           </div>
         </div>
